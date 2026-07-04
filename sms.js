df721ce35e6e06f1435dd274601d2f1fa7c8ed96
muzans <120549109+araimedev@users.noreply.github.com>
2026-07-04 21:56:02 +0700

    Update sms.js

--- sms.js
+++ sms.js
@@ -6,7 +6,7 @@ import { db } from './firebase.js';
 // ==========================================
 const PROVIDERS = {
     "smscode": { name: "COD", url: "https://sms.aam-zip.workers.dev" },
-    "herosms": { name: "HER", url: "https://hero.aam-zip.workers.dev" },
+    "herosms": { name: "HER", url: "https://hero.hajirnasrullah6.workers.dev" },
    "otpcepat": { name: "OTC", url: "https://cepat.aam-zip.workers.dev" },
     "svco": { name: "SVC", url: "https://svco.hajirnasrullah6.workers.dev" }
  // Ganti dengan URL Worker Nixpoin Anda
